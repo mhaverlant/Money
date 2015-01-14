5eef546574810436c2d760b3444c29716051b2c4
mhaverlant <matthiashaverlant@gmail.com>
2015-01-14 16:47:57 +0100

    Ajout du test equals et modification du test times

--- test/DollarTest.java
+++ test/DollarTest.java
@@ -10,10 +10,14 @@ public class DollarTest {
     public void testMultiplication(){
         Dollar five= new Dollar();
         five.setValue(5);
-        Dollar product= five.times(2);
-        assertEquals(10,product.getValue());
-        product=five.times(3);
-        assertEquals(15, product.getValue());
+        
+        Dollar test = new Dollar();
+        test.setValue(10);
+        assertEquals(test, five.times(2));
+
+        Dollar foistrois=new Dollar();
+        foistrois.setValue(15);
+        assertEquals(foistrois, five.times(3));
     }
 
     @Test
